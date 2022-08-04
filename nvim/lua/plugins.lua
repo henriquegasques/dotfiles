224f8467f82e53c477255e4553cd6f70518d8e31
Henrique Gasques <h.gasques@paywith.com>
2022-08-04 19:53:50 -0300

    Small refactoring

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,24 +1,33 @@
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
-return require('packer').startup(function(use)
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then return end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+return packer.startup(function(use)
   use 'wbthomason/packer.nvim' -- Packer can manage itself
+  use 'kyazdani42/nvim-web-devicons' -- Required by some plugins
 
-  use {
-    "mcchrish/zenbones.nvim", requires = "rktjmp/lush.nvim"
-  }
+  -- Colorschemes
+  use { "mcchrish/zenbones.nvim", requires = "rktjmp/lush.nvim" }
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+  use 'nvim-lualine/lualine.nvim'
+  use 'kyazdani42/nvim-tree.lua'
 
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+  -- TODO:
+  -- maybe install telescope
+  -- install some git plugin
 end)
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,7 +1,7 @@
 require("nvim-tree").setup({
   view = {
     adaptive_size = true,
-    -- mappings = { list = { { key = "u", action = "dir_up" }}}
+    -- mappings = { list = { { key = "some key", action = "some nvim-tree action" }}}
   },
   filters = {
     dotfiles = true,
