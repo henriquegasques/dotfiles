5f44cc56546dfa40fcfdad1d152c9aad6fa53acf
Henrique Gasques <h.gasques@paywith.com>
2022-08-06 15:37:11 -0300

    [Nvim] Some small config changes

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,7 +1,9 @@
 require("nvim-tree").setup({
   view = {
     adaptive_size = true,
-    -- mappings = { list = { { key = "some key", action = "some nvim-tree action" }}}
+     mappings = {
+       list = {{ key = "x", action = "close_node" }},
+     },
   },
   filters = {},
 })
@@ -17,7 +19,7 @@ vim.diagnostic.config({ virtual_text = true })
 require('gitsigns').setup {
   -- Overrides default signs for add and change
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add    = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
 }
