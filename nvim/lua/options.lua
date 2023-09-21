c9fcfdc55dd2035687e0072cd50065412db19fb7
Henrique Gasques <henriquegasques@gmail.com>
2023-09-21 13:03:26 -0300

    [NVim] Some nvim option changes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,33 +1,32 @@
 local options = {
   -- cmdheight   = 2,              -- more space in the neovim command line for displaying messages
-  backup         = false,          -- creates a backup file
+  backup         = false,          -- Don't create backup files
   clipboard      = "unnamedplus",  -- allows neovim to access the system clipboard
   conceallevel   = 0,              -- so that `` is visible in markdown files
   cursorline     = true,           -- highlight the current line
   expandtab      = true,           -- convert tabs to spaces
+  tabstop        = 4,              -- insert 4 spaces for a tab
+  smartindent    = true,           -- make indenting smart again
+  shiftwidth     = 0,              -- the number of spaces inserted for each indentation
   fileencoding   = "utf-8",        -- the encoding written to a file
   guicursor      = "",             -- cursor block in all modes
   ignorecase     = true,           -- ignore case in search patterns
   incsearch      = true,           -- incrementally highlight search matches as you type
   list           = true,
   number         = true,           -- set numbered lines
-  numberwidth    = 2,              -- set number column width to 2 {default 4}
-  pumheight      = 10,             -- pop up menu height
   relativenumber = true,           -- set relative numbered lines
-  scrolloff      = 15,             -- scroll offset
-  shiftwidth     = 0,              -- the number of spaces inserted for each indentation
+  numberwidth    = 1,              -- set number column width to 2 {default 4}
+  pumheight      = 10,             -- pop up menu height
+  scrolloff      = 20,             -- scroll offset
   showmode       = false,          -- we don't need to see things like -- INSERT -- anymore
   showtabline    = 0,              -- Never show tabs/buffers
-  sidescrolloff  = 15,             -- side scroll offset
-  signcolumn     = "yes",          -- always show the sign column, otherwise it would shift the text each time
+  signcolumn     = "yes",          -- always show sign column, otherwise it would shift the text each time
   smartcase      = true,           -- smart case
-  smartindent    = true,           -- make indenting smart again
-  splitbelow     = true,           -- force all horizontal splits to go below current window
-  splitright     = true,           -- force all vertical splits to go to the right of current window
-  swapfile       = false,          -- creates a swapfile
-  tabstop        = 4,              -- insert 4 spaces for a tab
-  termguicolors  = true,           -- set term gui colors (most terminals support this)
-  timeoutlen     = 100,            -- time to wait for a mapped sequence to complete (in milliseconds)
+  splitbelow     = true,           -- horizontal splits go below current window
+  splitright     = true,           -- vertical splits go to the right of current window
+  swapfile       = false,          -- Don't creates swapfiles
+  termguicolors  = true,           -- set term gui colors
+  timeoutlen     = 200,            -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile       = true,           -- enable persistent undo
   updatetime     = 300,            -- faster completion (4000ms default)
   wrap           = true,           -- display lines as one long line
@@ -41,7 +40,7 @@ end
 vim.opt.listchars = {
   trail = "•",
   eol   = "↲",
-  tab   = "» ",
+  -- tab   = "» ",
 }
 
 vim.cmd [[
